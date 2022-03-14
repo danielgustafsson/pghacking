02b6e3c86b578bddb213955c29757af528213b03
Daniel Gustafsson <daniel@yesql.se>
2022-03-14 14:24:09 +0100

    Provide a GUC for temporarily ignoring event triggers
    In order to allow troubleshooting and repair without the need for
    restarting in single-user mode, allow to ignore event triggers per
    session.

--- src/backend/utils/misc/guc.c
+++ src/backend/utils/misc/guc.c
@@ -46,6 +46,7 @@
 #include "catalog/pg_authid.h"
 #include "catalog/storage.h"
 #include "commands/async.h"
+#include "commands/event_trigger.h"
 #include "commands/prepare.h"
 #include "commands/tablespace.h"
 #include "commands/trigger.h"
@@ -562,6 +563,12 @@ static const struct config_enum_entry wal_compression_options[] = {
 	{"no", WAL_COMPRESSION_NONE, true},
 	{"1", WAL_COMPRESSION_PGLZ, true},
 	{"0", WAL_COMPRESSION_NONE, true},
+	{NULL, 0, false}
+};
+
+static const struct config_enum_entry ignore_event_trigger_options[] = {
+	{"none", IGNORE_EVENT_TRIGGER_NONE, false},
+	{"all", IGNORE_EVENT_TRIGGER_ALL, false},
 	{NULL, 0, false}
 };
 
@@ -4901,6 +4908,16 @@ static struct config_enum ConfigureNamesEnum[] =
 		},
 		&wal_compression,
 		WAL_COMPRESSION_NONE, wal_compression_options,
+		NULL, NULL, NULL
+	},
+
+	{
+		{"ignore_event_trigger", PGC_SUSET, CLIENT_CONN_STATEMENT,
+			gettext_noop("Disable event triggers during the session."),
+			NULL
+		},
+		&ignore_event_trigger,
+		IGNORE_EVENT_TRIGGER_NONE, ignore_event_trigger_options,
 		NULL, NULL, NULL
 	},
 
